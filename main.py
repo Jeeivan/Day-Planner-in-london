0a2ec36536689b1c32266331c04cb888aeda4016
Jeeivan <jeeivansri@gmail.com>
2023-05-13 15:57:10 +0100

    Added dictionaries for restaurant cuisines, prices and nearest tube stations

---
+++ main.py
@@ -0,0 +1,38 @@
+restaurants = ("Tayyabs", "Rasa", "Dishoom", "Xi' and impression", "Silk Road", "New Ming", "Trullo", "Manteca", "Ciao Bella")
+
+restaurant_cuisine = {
+    "Tayyabs": "Indian",
+    "Rasa": "Indian",
+    "Dishoom": "Indian",
+    "Xi'an Impression": "Chinese",
+    "Silk Road": "Chinese",
+    "New Ming": "Chinese",
+    "Trullo": "Italian",
+    "Manteca": "Italian",
+    "Ciao Bella": "Italian"
+}
+
+restaurant_prices = {
+    "Tayyabs" : "£20-30",
+    "Rasa" : "£15-25",
+    "Dishoom" : "£20-30",
+    "Xi' and impression" : "£15-25",
+    "Silk Road" : "£15-25",
+    "New Ming" : "£10-20",
+    "Trullo" : "£30-40",
+    "Manteca" : "£40-50",
+    "Ciao Bella" : "£20-30",
+
+}
+
+restaurant_tube_stations = {
+    "Tayyabs": "Whitechapel",
+    "Rasa": "St. John's Wood",
+    "Dishoom": "Covent Garden",
+    "Xi'an Impression": "Highbury & Islington",
+    "Silk Road": "London Bridge",
+    "New Ming": "Abbey Wood",
+    "Trullo": "Highbury & Islington",
+    "Manteca": "Barbican",
+    "Ciao Bella": "Tottenham Court Road"
+}
